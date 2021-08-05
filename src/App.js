0cb05fd8080484b58f364ed20b610fc5cdc6d554
BenjiCoder02 <benkoshy02@gmail.com>
2021-08-05 12:06:55 -0400

    added comments explaining code

--- src/App.js
+++ src/App.js
@@ -6,24 +6,33 @@ import Coin from "./Coin"
 
 function App() {
 
+  {/* Setting initial state for the input search bar*/}
+
   const [coins, setCoins] = useState([])
   const [search, setSearch] = useState("")
 
+  { /*
+    1. using useEffect to make api call on load 
+    2. using axios to make api calls to coingecko
+    3. setting interval to make calls every 2 seconds.*/}
   useEffect(() => {
     setInterval(function () {
-      axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=50&page=1&sparkline=false')
         .then(res => {
-          setCoins(res.data);
+          setCoins(res.data)
+          
         })
         .catch(error => console.log(error));
-    }, 5000)
+    }, 2000)
 
   }, [])
 
+  {/* This function will handle changes to input state*/}
   const handleChange = e => {
     setSearch(e.target.value)
   }
 
+  {/* This function filters the displayed coins based on the search field */}
   const filteredCoins = coins.filter(coin =>
     coin.name.toLowerCase().includes(search.toLocaleLowerCase()))
 
@@ -34,9 +43,14 @@ function App() {
         <form>
           <input type="text" placeholder="Search" className="coin-input" onChange={handleChange} />
         </form>
-      </div>
+        
+         </div>
+
+{/* Function call that maps only coins matching the search term */}
       {filteredCoins.map(coin => {
+        {/* Passing coin API data to the coin component using props*/}
         return (
+         
           <Coin
             key={coin.id}
             name={coin.name}
--- src/Coin.jsx
+++ src/Coin.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Coin.css"
 
+//Coin component that recieves API data from the parent component
+
 const Coin = (({ name, image, symbol, price, volume, priceChange, marketCap }) => {
     return (
         <div className="coin-container">
@@ -13,8 +15,11 @@ const Coin = (({ name, image, symbol, price, volume, priceChange, marketCap }) =
                 </div>
                 <div className="coin-data">
                     <p className="coin-price">${price}</p>
+                    {/* using toLocaleString to add commas for currency formatting*/}
+
                     <p className="coin-volume">${volume.toLocaleString()}</p>
                     {priceChange < 0 ? (
+                       /* adding classes for price changes and fixing the change percentage to 2 decimal points*/
                         <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
                     ) : (
                         <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
